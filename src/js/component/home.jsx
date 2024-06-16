1a17cdc83f137b0816d1759e23f5b2607faf28b3
My'lik Kerley <159506912+KerleyCode@users.noreply.github.com>
2024-06-16 16:24:25 -0500

    Initial commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+//create your first component
+const Home = () => {
+	const [ inputValue, setInputValue ] = useState("");
+	const [todos, setTodos] = useState([]);
+
+
+	return (
+		<div className="container">
+			<h1>My Todos </h1>
+			<ul>
+			 	<li>
+					<input 
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyPress={(e) => {				
+							if (e.key === "Enter") {
+								setTodos(todos.concat(inputValue));
+							}
+						}}
+
+						placeholder="What do you need to do?"></input>
+				</li>
+				{todos.map((item, index) => (
+					<li>
+						{item} <i class="fas fa-trash-alt" onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i>
+					</li>
+				))}
+			</ul>
+			<div>{todos.length} tasks</div>
+		</div>
+	);
+};
+
+export default Home;
